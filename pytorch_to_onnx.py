ef9f5f42f3894cd4dfcb8ab3cc3c8fc07613bea5
MostafaTheReal <reachmostafa.m@gmail.com>
2020-07-22 19:57:48 -0400

    cleaning code

--- pytorch_to_onnx.py
+++ pytorch_to_onnx.py
@@ -1,23 +1,37 @@
 import torch
-from models import *
+import sys
+import onnx
+import os
+import argparse
+import numpy as np
+from models import Yolov4
 
-def export_onnx_model(model, input_shape, onnx_path, input_names=None, output_names=None, dynamic_axes=None):
-    inputs = torch.ones(*input_shape)
-    model(inputs)
-    torch.onnx.export(model, inputs, onnx_path, input_names=input_names, output_names=output_names, dynamic_axes=dynamic_axes)
+def export_onnx_model(onnx_path, batches, num_classes, input_h, input_w):
+    model = load_model_weight(onnx_path, num_classes)
+    inputs = torch.randn((batches, 3, input_h, input_w), requires_grad=True)
+    print("------------exporting")
 
-def load_model_weight(model, model_path):
-    model.load_state_dict(torch.load(model_path))
-    model = model.eval()
+    onnx_name = "Yolov4_{}batches_{}classes.onnx".format(batches, num_classes)
+
+    torch.onnx.export(model, inputs, onnx_name, export_params=True, opset_version=11, do_constant_folding=True, input_names=['input'], output_names=['boxes', 'confs'], dynamic_axes=None)    
+    print("------------done exporting")
+
+def load_model_weight(model_path, num_classes):
+    model = Yolov4(n_classes=num_classes) 
+    print("------------loading model")
+    model.load_state_dict(torch.load(model_path, map_location=torch.device('cuda')))
+    print("------------done loading")
     return model
 
 
+
 if __name__=="__main__":
-    model_path = "/home/mostafathereal/Desktop/Hello_Raccoon/Yolov4_epoch51.pth"
-    model = Yolov4(n_classes=1)
-    model = load_model_weight(model, model_path)
-    # for name, v in model.named_parameters():
-    #     print(name)
-    input_shape = (1, 3, 416, 416)
-    onnx_path = "Hello_raccoon.onnx"
-    export_onnx_model(model, input_shape, onnx_path)
+    
+    if (len(sys.argv) != 2):
+        print("Usage: pytorch_to_onnx.py <model_weights_path>")
+        sys.exit()
+    model_path = sys.argv[1]
+    
+    export_onnx_model(model_path, 1, 1, 416, 416)
+
+
